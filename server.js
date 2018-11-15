ad6747705b679fc74978d94537f1d6629fe589ea
xueshangbin <963026270@qq.com>
2018-11-15 18:24:51 +0800

    js请求

--- server.js
+++ server.js
@@ -26,17 +26,17 @@ var server = http.createServer(function(request, response){
     response.setHeader('Content-Type', 'text/css; charset=utf-8')
     response.write('body{background-color: #ddd;}h1{color: red;}')
     response.end()
-  }else if(path == '/script.js'){
+  }else if(path == '/main.js'){
     response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
     response.write('alert("这是JS执行的")')
     response.end()
-  }else if(path == '/index.html'){
+  }else if(path == '/'){
     response.setHeader('Content-Type', 'text/html; charset=utf-8')
     response.write('<!DOCTYPE>\n<html>'  + 
       '<head><link rel="stylesheet" href="/style.css">' +
       '</head><body>'  +
       '<h1>你好</h1>' +
-      '<script src="/script.js"></script>' +
+      '<script src="/main.js"></script>' +
       '</body></html>')
     response.end()
   }else{
